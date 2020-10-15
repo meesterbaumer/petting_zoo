12377ebb6bd2949ce384cfc05d95ad38f3a95c9f
Michael Baumer <meeesterbaumer@gmail.com>
2020-10-15 16:39:31 -0500

    shifts added

--- models/slither.py
+++ models/slither.py
@@ -1,51 +1,75 @@
 from datetime import date
 
 class Viper():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.date_added = date.today()
         self.slithering = True
 
-vicky_viper = Viper("Vicky", "Viper")
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
+
+vicky_viper = Viper("Vicky", "Viper", "Rat")
 print(vicky_viper.name)
+print(vicky_viper.feed())
 
 class Copperhead():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.date_added = date.today()
         self.slithering = True
 
-connie_copperhead = Copperhead("Connie", "Copperhead")
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
+
+connie_copperhead = Copperhead("Connie", "Copperhead", "Rat")
 print(connie_copperhead.name)
+print(connie_copperhead.feed())
 
 class Worm():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.date_added = date.today()
         self.slithering = True
 
-willy_worm = Worm("Willy", "Worm")
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
+
+willy_worm = Worm("Willy", "Worm", "Dirt")
 print(willy_worm.name)
+print(willy_worm.feed())
 
 class Leech():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.date_added = date.today()
         self.slithering = True
 
-leslie_leech = Leech("Leslie", "Leech")
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
+
+leslie_leech = Leech("Leslie", "Leech", "Blood")
 print(leslie_leech.name)
+print(vicky_viper.feed())
 
 class Rattlesnake():
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
+        self.food = food
         self.date_added = date.today()
         self.slithering = True
 
-rodney_rattlesnake = Rattlesnake("Rodney", "Rattlesnake")
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
+
+rodney_rattlesnake = Rattlesnake("Rodney", "Rattlesnake", "Rat")
 print(rodney_rattlesnake.name)
